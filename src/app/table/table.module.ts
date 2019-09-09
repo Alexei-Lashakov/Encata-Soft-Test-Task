c53115a57ba8ecc8af9c00c31e7f55c1f4b3051b
Alexei-Lashakov <alexeilashakov@gmail.com>
2019-09-10 00:37:32 +0300

    Added implemention of TableComponent with sorting and filtering

--- src/app/table/table.module.ts
+++ src/app/table/table.module.ts
@@ -1,12 +1,17 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
+import { MatTableModule, MatSortModule, MatInputModule, MatFormFieldModule } from '@angular/material';
 import { TableComponent } from './table/table.component';
 
 @NgModule({
   declarations: [TableComponent],
   imports: [
     CommonModule,
+    MatTableModule,
+    MatSortModule,
+    MatFormFieldModule,
+    MatInputModule,
     RouterModule.forChild([
       {path: '', component: TableComponent}
     ])
